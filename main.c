a5f42ebb067185115e0e0b5789913996291ce3a9
espoupou <espoirativon@gmail.com>
2023-05-10 02:34:38 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - simple shell entry point
+ * @ac: argument count
+ * @av: argument vecto
+ * Return: always 0
+ */
+
+int main(int ac, char **av)
+{
+	printf("($) ");
+	return (0);
+}
