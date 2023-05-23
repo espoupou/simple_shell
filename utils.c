5398749b9bb54c2621e18332255bb4de6d3ff0a2
espoupou <126259211+espoupou@users.noreply.github.com>
2023-05-23 18:10:21 +0100

    Update utils.c: move cat_keyval to utils.c

--- utils.c
+++ utils.c
@@ -64,6 +64,27 @@ int _putchar(char c)
  * Return: the dest string
  */
 
+/**
+ * cat_keyenv - create env string "key=val"
+ * @env: env var pointer
+ * @key: key
+ * @val: value
+ * Return: pointer to string
+ */
+
+char *cat_keyval(char *key, char *val)
+{
+	char *env;
+
+	env = malloc(sizeof(char) * (_strlen(key) + _strlen(val) + 2));
+	_strcpy(env, key);
+	_strcat(env, "=");
+	_strcat(env, val);
+	_strcat(env, "\0");
+
+	return (env);
+}
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
