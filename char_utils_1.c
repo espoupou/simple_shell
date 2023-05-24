0f53ed287d70e42834e529035861bd9861d902e7
espoupou <espoirativon@gmail.com>
2023-05-24 16:26:44 -0700

    string implementing

--- char_utils_1.c
+++ char_utils_1.c
@@ -68,3 +68,39 @@ char *_strtok(char str[], const char *delim)
 		return (NULL);
 	return (str_start);
 }
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: pointer to first occurence or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] && s[i] != c; i++)
+		;
+
+	if (s[i] == c)
+		return (s + i);
+	return (NULL);
+}
+
+/**
+ * _strspn -  gets the length of a prefix substring.
+ * @s: the string
+ * @accept: acceptable byte
+ * Return: the length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (s[i] && strchr(accept, s[i]))
+		i++;
+
+	return (i);
+}
